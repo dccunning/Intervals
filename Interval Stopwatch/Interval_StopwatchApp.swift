dd1ee7134402db7ce8d314e951f6a63903eeb8ba
Dimitri Cunning <dcunning@bunq.com>
2024-02-22 00:21:37 +0100

    Initial Commit

---
+++ Interval Stopwatch/Interval_StopwatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Interval_StopwatchApp.swift
+//  Interval Stopwatch
+//
+//  Created by Dimitri Cunning on 22/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct Interval_StopwatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
